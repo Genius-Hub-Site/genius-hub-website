25a8966b8c0464e2c8ef8063aa20689652c29a82
nshabiirwe kizzah <nksiha.1601@gmail.com>
2023-02-22 15:38:00 +0300

    Updates 2

---
+++ js/mail_two.js
@@ -0,0 +1,33 @@
+
+(function() {
+    emailjs.init("QUhA5Rtm9FlJ9u1WI");
+  
+    const form = document.getElementById("contact-form-two");
+    const submitButton = document.getElementById("submit-btn-two");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent the form from submitting normally
+  
+      // Display a loading spinner on the submit button
+      submitButton.disabled = true;
+      submitButton.innerHTML = '<span class="submit-spinner"></span>Sending...';
+  
+      // Send the form data via EmailJS
+      emailjs.sendForm("service_vzwdgjy", "template_buvs3sb", this)
+        .then(function(response) {
+          console.log("SUCCESS!", response.status, response.text);
+          alert("Your message has been sent successfully.");
+          form.reset(); // Clear the form fields
+        }, function(error) {
+          console.log("FAILED...", error);
+          alert("Sorry, there was a problem sending your message. Please try again later.");
+        })
+        .finally(function() {
+          // Hide the loading spinner on the submit button
+          submitButton.disabled = false;
+          submitButton.innerHTML = "Submit";
+        });
+    });
+  })();
+  
+  
