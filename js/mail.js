55ec9f521359d43a9a0e90fd2a5658812b5e19b1
nshabiirwe kizzah <nksiha.1601@gmail.com>
2023-03-30 14:16:49 +0300

    updates

--- js/mail.js
+++ js/mail.js
@@ -1,27 +1,52 @@
 (function() {
-  emailjs.init("QUhA5Rtm9FlJ9u1WI");
-
-  const form = document.getElementById("contact-form");
-  const submitButton = document.getElementById("submit-btn");
-
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    submitButton.disabled = true;
-    submitButton.innerHTML = '<span class="submit-spinner"></span>Sending...';
-
-    emailjs.sendForm("service_vzwdgjy", "template_buvs3sb", this)
-      .then(function(response) {
-        console.log("SUCCESS!", response.status, response.text);
-        alert("Your message has been sent successfully.");
-        form.reset(); // Clear the form fields
-      }, function(error) {
-        console.log("FAILED...", error);
-        alert("Sorry, there was a problem sending your message. Please try again later.");
+    emailjs.init("qexGeuo7xN7wqmIt-")
+    document.getElementById('submit-btn').addEventListener('click', function(event) {
+      event.preventDefault(); // prevent default form submission behavior
+  
+      document.getElementById('submit-btn').innerHTML = '<span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>Sending...';
+  
+      // Get the form data
+      var form = document.getElementById('contact-form');
+      var job = form.elements['job'].value;
+      var name = form.elements['name'].value;
+      var email = form.elements['email'].value;
+      var phone = form.elements['phone'].value;
+      var bio = form.elements['bio'].value;
+  
+      // Use EmailJS to send the email
+      emailjs.send('6737&$2321)(*ttr_n5znxvd', 'template_nb9b09l', {
+        job: job,
+        name: name,
+        email: email,
+        phone: phone,
+        bio: bio
       })
-      .finally(function() {
-        submitButton.disabled = false;
-        submitButton.innerHTML = "Submit";
+      .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+  
+        // Reset the form
+        form.reset();
+  
+        // Hide the custom job field if it's visible
+        if (document.getElementById('custom-job-field-index').style.display !== 'none') {
+          document.getElementById('custom-job-field-index').style.display = 'none';
+        }
+  
+        // Show a success message
+        document.getElementById('submit-btn').innerHTML = 'Submitted!';
+        setTimeout(function() {
+          document.getElementById('submit-btn').innerHTML = 'Submit';
+        }, 3000);
+      }, function(error) {
+        console.log('FAILED...', error);
+  
+        // Show an error message
+        document.getElementById('submit-btn').innerHTML = 'Failed! Try again';
+        setTimeout(function() {
+          document.getElementById('submit-btn').innerHTML = 'Submit';
+        }, 3000);
       });
-  });
+   
+    });
+    
 })();
--- js/mail_two.js
+++ js/mail_two.js
@@ -1,31 +1,54 @@
-
 (function() {
-    emailjs.init("QUhA5Rtm9FlJ9u1WI");
+    emailjs.init("qexGeuo7xN7wqmIt-")
+    // Add event listener to the form submit button
+    document.getElementById('submit-btn-two').addEventListener('click', function(event) {
+      event.preventDefault(); // prevent default form submission behavior
   
-    const form = document.getElementById("contact-form-two");
-    const submitButton = document.getElementById("submit-btn-two");
+      // Display a loading spinner on the button
+      document.getElementById('submit-btn-two').innerHTML = '<span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>Sending...';
   
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
+      // Get the form data
+      var form = document.getElementById('contact-form-two');
+      var job = form.elements['job_two'].value;
+      var name = form.elements['name_two'].value;
+      var email = form.elements['email_two'].value;
+      var phone = form.elements['phone_two'].value;
+      var bio = form.elements['bio_two'].value;
   
-      submitButton.disabled = true;
-      submitButton.innerHTML = '<span class="submit-spinner"></span>Sending...';
+      // Use EmailJS to send the email
+      emailjs.send('6737&$2321)(*ttr_n5znxvd', 'template_lt11uhj962@$3~', {
+        job_two: job,
+        name_two: name,
+        email_two: email,
+        phone_two: phone,
+        bio_two: bio
+      })
+      .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
   
-      // Send the form data via EmailJS
-      emailjs.sendForm("service_vzwdgjy", "template_f8axbj5", this)
-        .then(function(response) {
-          console.log("SUCCESS!", response.status, response.text);
-          alert("Your message has been sent successfully.");
-          form.reset();
-        }, function(error) {
-          console.log("FAILED...", error);
-          alert("Sorry, there was a problem sending your message. Please try again later.");
-        })
-        .finally(function() {
-          submitButton.disabled = false;
-          submitButton.innerHTML = "Submit";
-        });
+        // Reset the form
+        form.reset();
+  
+        // Hide the custom job field if it's visible
+        if (document.getElementById('custom-job-field-index').style.display !== 'none') {
+          document.getElementById('custom-job-field-index').style.display = 'none';
+        }
+  
+        // Show a success message
+        document.getElementById('submit-btn_two').innerHTML = 'Submitted!';
+        setTimeout(function() {
+          document.getElementById('submit-btn-two').innerHTML = 'Submit';
+        }, 3000);
+      }, function(error) {
+        console.log('FAILED...', error);
+  
+        // Show an error message
+        document.getElementById('submit-btn-two').innerHTML = 'Failed! Try again';
+        setTimeout(function() {
+          document.getElementById('submit-btn-two').innerHTML = 'Submit';
+        }, 3000);
+      });
+   
     });
-  })();
-  
-  
+    
+})();
